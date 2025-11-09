ddbc791515b7317a04badee73e88942f1b896838
Nishil Soni <soninishil27@gmail.com>
2025-11-09 18:26:43 +0530

    Initial Commit

---
+++ TaskGrid/TaskGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskGridApp.swift
+//  TaskGrid
+//
+//  Created by Nishil Soni on 09/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
